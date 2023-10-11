70ca4a75b6fed501b800994786ecddca37f2f9a7
Abheeshu-Singh <asingh525kumar@gmail.com>
2023-10-11 14:10:17 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow[100],
+        body: const Center(
+          child: Column(
+            children: [
+              CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/images/goku.png')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
