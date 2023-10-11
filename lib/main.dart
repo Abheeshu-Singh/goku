3dce0ebf9f54ce35370c35f6fc81e47d521f8717
Abheeshu-Singh <asingh525kumar@gmail.com>
2023-10-11 21:42:27 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,66 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.yellow[100],
-        body: const Center(
-          child: Column(
-            children: [
-              CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage('assets/images/goku.png')),
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundColor: Colors.black45,
+              radius: 100,
+              backgroundImage: AssetImage('assets/images/goku.png'),
+            ),
+            const SizedBox(
+              height: 20,
+              width: 400,
+              child: Divider(
+                color: Colors.black38,
+              ),
+            ),
+            const Text(
+              'GOKU THE SAVIOUR',
+              style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+              width: 400,
+              child: Divider(
+                color: Colors.black38,
+              ),
+            ),
+            const Text(
+              'THE ALMIGHTY SUPER SAIYAN',
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black45,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+              width: 400,
+              child: Divider(
+                color: Colors.black38,
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.all(15),
+                color: Colors.yellowAccent[100],
+                child: ListTile(
+                  leading: Icon(
+                    Icons.power,
+                    color: Colors.black54,
+                  ),
+                  title: Text(
+                    'KAMEHAMEHA FINAL SHOT',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ))
+          ],
         ),
       ),
     );
